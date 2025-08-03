6da673ac9304d3fd545e9f6b98adac7121acc94f
lutficaglayanusta <65968577+lutficaglayanusta@users.noreply.github.com>
2025-08-03 23:56:06 +0000

    Deploying to gh-pages from @ cinemania-teamb/goit-cinemania@2c74d3d0f4769cb6425b207b8ac66b3c2009b35b 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import{m as $,g as M}from"./assets/loader-bIkT6TQI.js";import"./assets/header-DLs29yVk.js";import{a as u,d as T}from"./assets/vendor-oKrFQJru.js";const y="31537460e1a179eb456191c366c87865",b="https://api.themoviedb.org/3",L="https://image.tmdb.org/t/p/",w=document.getElementById("weekly-trends"),E=`<svg class="icon" id="stars-empty" width="18" height="18">
+import{m as $,g as M}from"./assets/loader-CQ-Z8sbo.js";import"./assets/header-DDFMVBHF.js";import{a as u,d as T}from"./assets/vendor-oKrFQJru.js";const y="31537460e1a179eb456191c366c87865",b="https://api.themoviedb.org/3",L="https://image.tmdb.org/t/p/",w=document.getElementById("weekly-trends"),E=`<svg class="icon" id="stars-empty" width="18" height="18">
           <use href="./icons.svg#icon-star-outline"></use>
         </svg>`,A=`<svg class="icon" id="stars-half" fill="#F87719"  width="18" height="18">
           <use href="./icons.svg#icon-star-half"></use>
